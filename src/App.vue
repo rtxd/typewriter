69ee6f557635ef59ef53150af9a6f83a9615e451
Alejandro Munoz <alej.munoz@gmail.com>
2025-04-22 21:59:15 +1000

    temp user status for devmode

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
+  <header>
+    <div class="status">
+      <div>User Status</div>
+      <div>Current: {{ user.data?.displayName }}</div>
+    </div>
+  </header>
   <main>
     <router-view />
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuthStore } from './stores/auth'
+
+const { user } = useAuthStore()
+</script>
+
+<style>
+.status {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 4rem;
+  right: 4rem;
+}
+</style>
